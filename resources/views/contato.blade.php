7beafb464a9b414f00cfc3e6014e2002f9297b0d
rodolfo <rodolfovillasboas@gmail.com>
2019-09-15 13:29:28 -0300

    correção e link de modal de login entre as views

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.master')
-    @section('content')
+@extends('modais')
 
-        <section>
-            <div class="fundo">
-                <div class="contact">
-                    <div class="contact-title text-center">
-                        <h4>Quer mais Informações? Ficou com duvida?<br>
-                                Entre em contato conosco</h4>
-                    </div> 
-                    <form action="#">
-                        <label for="">Nome</label>
-                        <input type="text">
-                        <label for="">E-mail</label>
-                        <input type="email">
-                        <label for="">Mensagem</label>
-                        <textarea name="" id="" cols="30" rows="10"></textarea>   
-                        <div class="contact-button"> 
-                            <button type="submit" name="botao">Enviar</button>                                        
-                        </div>
-                    </form>
-                </div>
+@section('content')
+
+<section>
+    <div class="fundo">
+        <div class="contact">
+            <div class="contact-title text-center">
+                <h4>Quer mais Informações? Ficou com duvida?<br>
+                    Entre em contato conosco</h4>
             </div>
-        </section>
-        
-    @endsection
+            <form action="#">
+                <label for="">Nome</label>
+                <input type="text">
+                <label for="">E-mail</label>
+                <input type="email">
+                <label for="">Mensagem</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <div class="contact-button">
+                    <button type="submit" name="botao">Enviar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+@endsection
--- resources/views/marcas.blade.php
+++ resources/views/marcas.blade.php
@@ -1,47 +1,56 @@
 @extends('layouts.master')
-    @section('content')
+@extends('modais')
+
+@section('content')
 
 <section class="servicos">
-<div class="text-center">
+    <div class="text-center">
         <h1 class="p-3 titulo-landing-page">Encontre o micro influenciador para sua campanha!</h1>
     </div>
 
-                <div class="container">
-                
-                <div>
-                    <div class="row align-items-center">
-                        <div class="col-12 col-md-4 d-flex justify-content-center">
-                        <img class="img-fluid p-0 d-block imagem-services" alt="Faça o cadastro de sua marca" title="anunciante1" src="image/anunciantes2.png" alt="">
-                        </div>
-                        <div class="col-md-8 servicos-texto">
-                            <h2>Faça o cadastro de sua marca</h2>
-                            <p class="m-0">Realize o cadastro de sua marca e conheça nossa plataforma de micro influencidores que vão aumentar
-                                o alcance e engajamento de sua coleção, conceito e estilo e o melhor de tudo para o público alvo que você sempre
-                                quis alcançar!
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row align-items-center">
-                        <div class="col-12 col-md-4 d-flex justify-content-center order-md-1">
-                        <img class="img-fluid p-0 d-block imagem-services" alt="Comece sua campanha neste exato momento" title="anunciante2" src="image/anunciantes1.png" alt="">
-                        </div>
-                        <div class="col-md-8 servicos-texto">
-                            <h2>Comece sua campanha neste exato momento!</h2>
-                            <p class="m-0">Com apenas alguns cliques realize sua campanha e receba notificações de todos os micro influenciadores
-                                que estão prontos para levar a sua marca para outro nível.</p>
-                        </div>
-                    </div>
-                    <div class="row align-items-center">
-                        <div class="col-12 col-md-4 d-flex justify-content-center">
-                        <img class="img-fluid p-0 d-block imagem-services" alt="Acompanhe os resultados na nossa plataforma" title="anunciante3" src="image/anunciantes3.png" alt="">
-                        </div>
-                        <div class="col-md-8 servicos-texto">
-                            <h2>Acompanhe os resultados na nossa plataforma</h2>
-                            <p>Não fique apenas aguardando relatórios mensais e acompanhe os resultados real time em nossa plataforma
-                            e não perca nenhuma novidade com seus <strong>micro influenciadores</strong> preferidos.</p>
-                        </div>
-                    </div>
+    <div class="container">
+
+        <div>
+            <div class="row align-items-center">
+                <div class="col-12 col-md-4 d-flex justify-content-center">
+                    <img class="img-fluid p-0 d-block imagem-services" alt="Faça o cadastro de sua marca"
+                        title="anunciante1" src="image/anunciantes2.png" alt="">
                 </div>
+                <div class="col-md-8 servicos-texto">
+                    <h2>Faça o cadastro de sua marca</h2>
+                    <p class="m-0">Realize o cadastro de sua marca e conheça nossa plataforma de micro influencidores
+                        que vão aumentar
+                        o alcance e engajamento de sua coleção, conceito e estilo e o melhor de tudo para o público alvo
+                        que você sempre
+                        quis alcançar!
+                    </p>
                 </div>
-            </section>
-    @endsection
+            </div>
+            <div class="row align-items-center">
+                <div class="col-12 col-md-4 d-flex justify-content-center order-md-1">
+                    <img class="img-fluid p-0 d-block imagem-services" alt="Comece sua campanha neste exato momento"
+                        title="anunciante2" src="image/anunciantes1.png" alt="">
+                </div>
+                <div class="col-md-8 servicos-texto">
+                    <h2>Comece sua campanha neste exato momento!</h2>
+                    <p class="m-0">Com apenas alguns cliques realize sua campanha e receba notificações de todos os
+                        micro influenciadores
+                        que estão prontos para levar a sua marca para outro nível.</p>
+                </div>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-12 col-md-4 d-flex justify-content-center">
+                    <img class="img-fluid p-0 d-block imagem-services" alt="Acompanhe os resultados na nossa plataforma"
+                        title="anunciante3" src="image/anunciantes3.png" alt="">
+                </div>
+                <div class="col-md-8 servicos-texto">
+                    <h2>Acompanhe os resultados na nossa plataforma</h2>
+                    <p>Não fique apenas aguardando relatórios mensais e acompanhe os resultados real time em nossa
+                        plataforma
+                        e não perca nenhuma novidade com seus <strong>micro influenciadores</strong> preferidos.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
